684ba551d7706d693fd417dbbc501bcd64bc8af8
ghostdizz <ghostdizz@protonmail.com>
2023-08-22 21:57:11 +0700

    Add python code

---
+++ 1768.Merge_Strings_Alternately.py
@@ -0,0 +1,15 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        length1 = len(word1)
+        length2 = len(word2)
+        
+        s = ""
+        for i in range(min(length1, length2)):
+            s += word1[i] + word2[i]
+        if length1 < length2:
+            s += word2[length1:]
+        elif length1 > length2:
+            s += word1[length2:]
+        return s
+
+print(Solution().mergeAlternately("ab", "pqrs"))
