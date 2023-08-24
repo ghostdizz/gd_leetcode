d55bea11f42ca38cc47978db2c44e94a02a774cb
ghostdizz <ghostdizz@protonmail.com>
2023-08-24 18:53:42 +0700

    Add python code

--- 263.Ugly_Number.py
+++ 263.Ugly_Number.py
@@ -1,17 +1,13 @@
 class Solution:
     def isUgly(self, n: int) -> bool:
+        if n == 0:
+            return False
         while n%2 == 0:
             n //= 2
-            if n == 0:
-                break
         while n%3 ==0:
             n //= 3
-            if n == 0:
-                break
         while n%5 ==0:
             n //= 5
-            if n == 0:
-                break
         if n == 1:
             return True
         return False
