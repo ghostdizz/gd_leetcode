89486896f0d10c66fd511a8521dbc518cb79ea38
ghostdizz <ghostdizz@protonmail.com>
2023-09-09 22:24:07 +0700

    Add python code

---
+++ 397.Integer_Replacement.py
@@ -0,0 +1,20 @@
+class Solution:
+    def __init__(self):
+        self.min_count = float('inf')
+
+    def recursion(self, n, count):
+        if n == 1: 
+            if count < self.min_count:
+                self.min_count = count
+            return
+        if n%2 == 0:
+            self.recursion(n//2, count + 1)
+        else:
+            self.recursion(n+1, count + 1)
+            self.recursion(n-1, count + 1)
+
+    def integerReplacement(self, n: int) -> int:
+        self.recursion(n, 0)
+        return self.min_count
+    
+print(Solution().integerReplacement(8))
