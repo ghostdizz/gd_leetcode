256e174bcdb1b00a62f004937d6dde3086fb62bb
ghostdizz <ghostdizz@protonmail.com>
2023-10-10 16:09:12 +0700

    Add python code

---
+++ 628.Maximum_Product_of_Three_Numbers.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def maximumProduct(self, nums: List[int]) -> int:
+        nums.sort()
+        a = nums[-3]*nums[-2]*nums[-1]
+        if nums[0]*nums[1]*nums[-1] > a:
+            return nums[0]*nums[1]*nums[-1]
+        return a
+
+print(Solution().maximumProduct([1,2,3,4]))
