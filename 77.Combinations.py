2e82a8e6372147135effde1da9cad5be345281ad
Peter John <ghostdizz@protonmail.com>
2023-10-28 19:47:20 +0700

    Add python code

--- 77.Combinations.py
+++ 77.Combinations.py
@@ -1,18 +1,6 @@
 from typing import List
+import itertools
 
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        ans = []
-        nums = [0] * k
-
-        def recursion(position, current):
-            if position == k:
-                ans.append(nums[:])
-                return 0
-            for i in range(current, n-k+position+2):
-                nums[position] = i
-                recursion(position+1, i+1)
-        
-        recursion(0, 1)
-        return ans
-    
+        return list(itertools.combinations(list(range(1,n+1)), k))
